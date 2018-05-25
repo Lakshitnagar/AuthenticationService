e4ce0a7316db749f4e7dee62cd9ba4228eb9df99
DESKTOP-5BSHC8B\Lakshit <lakshitnagar@gmail.com>
2018-05-25 22:05:30 +0530

    removed previous files

--- test/controllers/tests_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get test1" do
-    get tests_test1_url
-    assert_response :success
-  end
-
-end
