9df43191383b0dca09bcb526540566a372fc79d2
PERSISTENT\lakshit_nagar <lakshit_nagar@persistent.co.in>
2018-06-01 16:47:06 +0530

    removed unwanted files and changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_202306) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_06_01_111509) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
