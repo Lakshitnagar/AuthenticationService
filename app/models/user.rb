5c89b397ff771d6a3c2aa2fb1bec9badad6f883a
PERSISTENT\lakshit_nagar <lakshit_nagar@persistent.co.in>
2018-06-08 19:26:36 +0530

    mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token, :activation_token
 	before_save :downcase_email
+	before_create :create_activation_digest
+
 	validates :name,
 				presence: true,
 				length: {maximum:50}
@@ -37,9 +39,10 @@ class User < ApplicationRecord
 	end
 
 	# Returns true if the given token matches the digest.
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	# Forgets a user.
