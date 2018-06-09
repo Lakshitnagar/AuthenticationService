4df9e156ec6b3cae10a5f0523d4f2a02f8c32081
DESKTOP-5BSHC8B\Lakshit <lakshitnagar@gmail.com>
2018-06-09 11:58:37 +0530

    error resolved for activation edit action

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000' # Don't use this literally; use your local dev host instead
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
